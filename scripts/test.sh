b02919f24223e7dd6f336a74be35224334210b42
theque5t <theque5t@users.noreply.github.com>
2021-12-18 13:10:55 -0600

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,10 +5,11 @@ echo $DetectorPID                                # Output PID
 sleep 5                                          # Let run for 5 seconds
 cat Log4jDetector/log/app.log                    # Output log
 line1="$(head -n 1 Log4jDetector/log/app.log)"   # Get line 1 from log
+echo "${line1: -6:5}"
 if [[ "${line1: -6:5}" == "Start" ]]; then       # Test if started successfully
     echo "Pass"
-    echo 0
+    exit 0
 else
     echo "Fail"
-    echo 1
+    exit 1
 fi
