29aa77b00c5cbd7827baad8daea17fb46bae9a11
theque5t <theque5t@users.noreply.github.com>
2021-12-18 12:44:18 -0600

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,12 @@ java -jar build/libs/Log4jDetector.jar &         # Start it in the background
 DetectorPID=$!                                   # Record PID
 echo $DetectorPID                                # Output PID
 sleep 5                                          # Let run for 5 seconds
-kill $DetectorPID                                # Stop it
 cat Log4jDetector/log/app.log                    # Output log
 line1="$(head -n 1 Log4jDetector/log/app.log)"   # Get line 1 from log
-[[ "${line1: -6:5}" == "Start" ]] && echo "Pass"; exit 0; || echo "Fail"; exit 1;
+if [[ "${line1: -6:5}" == "Start" ]]; then       # Test if started successfully
+    echo "Pass"
+    echo 0
+else
+    echo "Fail"
+    echo 1
+fi
