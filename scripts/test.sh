3f86f922f770925d4c4676449420b056ed8e43ab
theque5t <theque5t@users.noreply.github.com>
2021-12-18 16:21:29 -0600

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Started jar..."
-echo "5" | java -jar "build/libs/Log4jDetector${VERSION:+-VERSION}.jar" &     # Start in the background with 5 second interval
+nohup bash -c 'echo "5" | java -jar "build/libs/Log4jDetector${VERSION:+-VERSION}.jar"' &     # Start in the background with 5 second interval
 DetectorPID=$!                                                                # Record PID
 (sleep 15 && kill $DetectorPID) || echo "exit 1"                              # Let run for 15 seconds and stop, or fail if process does not exist
 echo "Stopped jar..."
